461116f0684ba16d3ad2db912c761bcb37cda568
sangho hwang <gommg5248@daum.net>
2021-07-11 01:32:14 +0900

    07_11_dfs_음료수얼려먹기

---
+++ 07_11/07_11_DFS_음료수얼려먹기.py
@@ -0,0 +1,32 @@
+n, m = map(int,input().split())
+
+#2차원 리스트 맵 정보 입력
+graph=[]
+for i in range(n):
+    graph.append(list(map(int,input())))
+
+#dfs로 특정 노드 방문 후 연결된 모든 노드 방문
+def dfs(x,y):
+    #주어진 범위를 벗어나면 즉시 종료
+    if x<=-1 or x>=n or y<=-1 or y>=m:
+        return False
+    #현재 노드를 아직 미방문이라면
+    if graph[x][y] == 0:
+        # 해당 노드 방문 처리
+        graph[x][y]=1
+        #상하좌우 위치 모두 재귀호출
+        dfs(x-1,y)
+        dfs(x,y-1)
+        dfs(x+1,y)
+        dfs(x,y+1)
+        return True
+    return False
+
+#모든 노드에 대해 음료수 채우기
+result=0
+for i in range(n):
+    for j in range(m):
+        #현 위치 dfs 수행
+        if dfs(i, j) == True:
+            result+=1
+print(result)
