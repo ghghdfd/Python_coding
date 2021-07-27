7554d1eb42a8d4d1bed31fa20b0e0f08b55e3ea4
sangho hwang <gommg5248@daum.net>
2021-07-28 01:10:31 +0900

    07_28_update

---
+++ 07_28/programmers_입국심사.py
@@ -0,0 +1,24 @@
+##프로그래머스_입국심사
+
+def solution(n, times):
+    answer = 0
+    
+    leng=len(times)
+    left=1
+    right=(leng+1)*max(times)
+    
+    while left<=right:
+        mid=(left+right)//2
+        
+        count=0
+        for time in times:
+            count += mid//time
+            if count >= n: break
+                
+        if count >= n:
+            answer=mid
+            right=mid-1
+            
+        elif count < n:
+            left=mid+1
+    return answer
