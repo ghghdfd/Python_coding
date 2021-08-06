e2d7a7daa8d5c99224558d48ec4d4c4d961d4452
sangho hwang <gommg5248@daum.net>
2021-08-06 23:34:36 +0900

    08_06_update

---
+++ 08_06/programmers_여행경로.py
@@ -0,0 +1,30 @@
+##여행경로
+
+
+from collections import defaultdict
+
+def solution(tickets):
+    answer = []
+    routes = defaultdict(list)
+
+    for ticket in tickets:
+        routes[ticket[0]].append(ticket[1])
+
+    for key in routes.keys():
+        routes[key].sort(reverse=True)
+
+    stack = ['ICN']
+    while stack:
+        tmp = stack[-1]
+
+        if not routes[tmp]:
+            answer.append(stack.pop())
+        else:
+            stack.append(routes[tmp].pop())
+    answer.reverse()
+
+    return answer
+
+tickets=[["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
+solution(tickets)
+
