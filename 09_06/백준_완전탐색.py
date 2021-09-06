fde84ca4368773fa052c68bec216d84244db3761
sangho hwang <gommg5248@daum.net>
2021-09-06 19:18:55 +0900

    09_06_update

---
+++ 09_06/백준_완전탐색.py
@@ -0,0 +1,18 @@
+#완전탐색
+
+n = int(input())
+min = n
+
+for _ in str(n):
+    min -= 9
+    
+    if min <= 0:
+        min = 1
+        break
+        
+for num in range(min, n):
+    if num + sum([int(i) for i in str(num)]) == n:
+        print(num)
+        exit(0)
+        
+print(0)
